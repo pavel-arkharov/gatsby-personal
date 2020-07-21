a274205a07e22f109655ebdcff774f9a02216a16
Argali <arkharovp@gmail.com>
2020-07-21 17:30:10 +0300

    Added github for contacts

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,55 @@
-import React from 'react'
-import Layout from '../components/layout'
-import Head from '../components/head'
+import React from "react"
+import Layout from "../components/layout"
+import Head from "../components/head"
 
 const ContactPage = () => {
-    return (
-        <Layout>
-        <Head title="Contact" />
-            <h1>Contact me</h1>
-                <p><a href="https://t.me/rkhrv" target="_blank" rel="noopener noreferrer">Telegram</a></p>
-                <p><a href = "mailto: arkharovp@gmail.com" target="_blank" rel="noopener noreferrer">Email</a></p>
-                <p><a href="https://api.whatsapp.com/send?phone=+358414748176" target="_blank" rel="noopener noreferrer">WhatsApp</a></p>
-                <p><a href="https://www.facebook.com/pasha.rharov" target="_blank" rel="noopener noreferrer">facebook</a></p>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head title="Contact" />
+      <h1>Contact me</h1>
+      <p>
+        <a href="https://t.me/rkhrv" target="_blank" rel="noopener noreferrer">
+          Telegram
+        </a>
+      </p>
+      <p>
+        <a
+          href="mailto: arkharovp@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Email
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://api.whatsapp.com/send?phone=+358414748176"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          WhatsApp
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://www.facebook.com/pasha.rharov"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          facebook
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://github.com/asigaru"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
+    </Layout>
+  )
 }
 
 export default ContactPage
